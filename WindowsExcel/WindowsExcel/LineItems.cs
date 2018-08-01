1511f49bebc3fb6c4d89cd0150c1fcaf7f6d18be
meloht <luohongtao621@163.com>
2018-08-02 07:12:45 +0800

    aa

---
+++ WindowsExcel/WindowsExcel/LineItems.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsExcel
+{
+    public class LineItems<T>
+    {
+        public List<string> XPoints { get; set; }
+
+        public List<T> YPoints { get; set; }
+
+        public DataTable Table { get; set; }
+
+        public string YLable { get; set; }
+
+        public string XLable { get; set; }
+    }
+}
